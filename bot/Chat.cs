7718ce60e537508eb4d61ed8d62efe511947bc3a
Matt Stevens <matt@dev-zilla.net>
2012-05-10 20:47:14 +0100

    Using Naarkie's bot as a base.

---
+++ bot/Chat.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using SteamKit2;
+
+namespace AgopBot
+{
+    class Chat : ICallbackHandler
+    {
+        public Chat()
+        {
+            Steam.AddHandler(this);
+        }
+
+        public static void Join(SteamID id)
+        {
+            id.AccountInstance = 0x80000;
+            id.AccountType = EAccountType.Chat;
+            Steam.Friends.JoinChat(id);
+        }
+
+        public static void Send(SteamID id, String message)
+        {
+            Steam.Friends.SendChatRoomMessage(id, EChatEntryType.ChatMsg, message);
+        }
+
+        public void HandleCallback(CallbackMsg msg)
+        {
+            if (msg.IsType<SteamUser.LoginKeyCallback>())
+                HandleLogin((SteamUser.LoginKeyCallback)msg);
+            if (msg.IsType<SteamUser.LoggedOffCallback>())
+                HandleLogoff((SteamUser.LoggedOffCallback)msg);
+            if (msg.IsType<SteamFriends.ChatEnterCallback>())
+                HandleChatEnter((SteamFriends.ChatEnterCallback)msg);
+            if (msg.IsType<SteamFriends.ChatMsgCallback>())
+                HandleChatMessage((SteamFriends.ChatMsgCallback)msg);
+            if (msg.IsType<SteamFriends.PersonaStateCallback>())
+                HandlePersonaStateChange((SteamFriends.PersonaStateCallback)msg);
+        }
+
+        public static void HandleLogin(SteamUser.LoginKeyCallback msg)
+        {
+            Steam.Friends.SetPersonaName("AgopBot");
+            Steam.Friends.SetPersonaState(EPersonaState.Online);
+            Join(103582791433166824);
+        }
+
+        public static void HandleLogoff(SteamUser.LoggedOffCallback msg)
+        {
+        }
+
+        public static void HandleChatEnter(SteamFriends.ChatEnterCallback msg)
+        {
+            if (msg.EnterResponse != EChatRoomEnterResponse.Success)
+                Console.WriteLine("Steam > Unable to enter this chatroom (" + msg.EnterResponse.ToString() + ").");
+        }
+
+        public static void HandleChatMessage(SteamFriends.ChatMsgCallback msg)
+        {
+            SteamID Sender = msg.ChatterID;
+            String SenderName = Steam.Friends.GetFriendPersonaName(Sender);
+            SteamID Room = msg.ChatRoomID;
+        }
+
+        public static void HandlePersonaStateChange(SteamFriends.PersonaStateCallback msg)
+        {
+            
+        }
+    }
+}
---
+++ bot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SteamKit2;
+
+namespace AgopBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Steam.Username = args[0];
+            Steam.Password = args[1];
+            Steam.AuthCode = "";
+            Steam.Connect();
+
+            bool isComplete = false;
+
+            Console.CancelKeyPress += (object s, ConsoleCancelEventArgs e) =>
+            {
+                isComplete = true;
+            };
+
+            while (!isComplete)
+            {
+                Steam.Update();
+                Thread.Sleep(1);
+            }
+
+            Steam.Shutdown();
+        }
+    }
+}
