3f43f9f8238ccc2145dadb56cbe0acc79acca78a
Dlaor <dlaory@gmail.com>
2012-05-13 16:22:40 +0200

    Added sudo restart, and sudo update (which doesn't work yet, this is just a proof of concept with the batch file)

---
+++ bot/Commands/CmdUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using SteamKit2;
+
+namespace AgopBot.Commands
+{
+    public class CmdUpdate : Command
+    {
+        public CmdUpdate() : base("update") { }
+
+        public override void Use(SteamID room, SteamID sender, string[] args)
+        {
+            Process.Start("update.bat");
+
+            Chat.Send(room, "Updating, be right back!");
+            Steam.Shutdown();
+        }
+    }
+}
