ad7bcab5e9b4348f87bdb325c1782ba00730c3f0
Dlaor <dlaory@gmail.com>
2012-05-17 21:48:41 +0200

    Added "sudo name [name]" command, which changes the nickname of the bot. It does this without notification, so that means we can make an IRC bot now!

---
+++ bot/Commands/CmdName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using SteamKit2;
+
+namespace AgopBot.Commands
+{
+    public class CmdName : Command
+    {
+        public CmdName() : base("name") { }
+
+        public override void Use(SteamID room, SteamID sender, string[] args)
+        {
+            string arg = String.Join("", args);
+
+            Steam.Friends.SetPersonaName(arg);
+            Chat.Send(room, string.Format("Name is now {0}.", arg));
+        }
+    }
+}
